60526a190a1e97f4cd0edabd5a344e4f5e5ad157
Suzy <susan.m.mullin@gmail.com>
2018-12-14 22:20:06 -0800

    added decrement feature

--- src/App.js
+++ src/App.js
@@ -21,6 +21,14 @@ class App extends Component {
     this.setState({ counters });
   };
 
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
   handleDelete = (counterId) => {
     const counters = this.state.counters.filter((c) => c.id !== counterId);
     this.setState({ counters: counters });
@@ -35,8 +43,6 @@ class App extends Component {
   };
 
   render() {
-    console.log('App - Rendered');
-
     return (
       <React.Fragment>
         <NavBar
@@ -48,6 +54,7 @@ class App extends Component {
             onReset={this.handleReset}
             onDelete={this.handleDelete}
             onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
           />
         </main>
       </React.Fragment>
